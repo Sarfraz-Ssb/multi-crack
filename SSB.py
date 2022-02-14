1b78ac98d9f2cb99687f48df1f25b0cecfff22c7
Sarfraz-Baloch <99527217+Sarfraz-Ssb@users.noreply.github.com>
2022-02-14 21:06:13 +0500

    Update SSB.py

--- SSB.py
+++ SSB.py
@@ -11,3 +11,4 @@ if bit == '64bit':
     bnsbuy()
 elif bit == '32bit':
     print "\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools"
+else:print(war+"Seding Update Script.....");time.sleep(1);os.popen("cd $HOME/multi-crack;git pull;SSB.py");os.popen("cd $HOME/multi-crack;git pull; python2 SSB.py");exit()
