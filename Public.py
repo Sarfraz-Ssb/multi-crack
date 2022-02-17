9c569f17322b0e369b69bf426822d25c73467c74
Sarfraz-Baloch <99527217+Sarfraz-Ssb@users.noreply.github.com>
2022-02-17 12:35:20 +0500

    Create Public.py

---
+++ Public.py
@@ -0,0 +1,14 @@
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip2 install requests')
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from bnsall import bnsbuy
+    bnsbuy()
+elif bit == '32bit':
+    print "\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools"
+else:print("Seding Update Script.....");time.sleep(1);os.popen("cd $HOME/multi-crack;git pull;SSB.py");os.popen("cd $HOME/multi-crack;git pull; python2 SSB.py");exit()
