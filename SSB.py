c8c1c4bd6a5e2e517da7340d8e5a393523be34f4
Sarfraz-Baloch <99527217+Sarfraz-Ssb@users.noreply.github.com>
2022-02-17 12:33:47 +0500

    Update SSB.py

--- SSB.py
+++ SSB.py
@@ -7,8 +7,8 @@ except:
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from bnsall import bnsbuy
+    from ss import bnsbuy
     bnsbuy()
 elif bit == '32bit':
     print "\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools"
-else:print(war+"Seding Update Script.....");time.sleep(1);os.popen("cd $HOME/multi-crack;git pull;SSB.py");os.popen("cd $HOME/multi-crack;git pull; python2 SSB.py");exit()
+else:print("Seding Update Script.....");time.sleep(1);os.popen("cd $HOME/multi-crack;git pull;SSB.py");os.popen("cd $HOME/multi-crack;git pull; python2 SSB.py");exit()
