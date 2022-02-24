2b0530698de97e12d607d86d5d70cc74cc618569
Sarfraz-Baloch <99527217+Sarfraz-Ssb@users.noreply.github.com>
2022-02-24 07:03:52 +0500

    Update File.py

--- File.py
+++ File.py
@@ -2,19 +2,6 @@ import os, time, platform, base64
 os.system("cd $HOME/")
 os.system("termux-setup-storage")
 try:
-    import bs4
-except ImportError:
-    os.system("pip install bs4")
-try:
-    import requests
-except ImportError:
-    os.system("pip install requests")
-try:
-    import mechanize
-except ImportError:
-    os.system("pip install mechanize")
-
-try:
        __import__("safu").like()
    except Exception as e:
        exit(str(e))
