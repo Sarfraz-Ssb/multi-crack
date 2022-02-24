05e91a53e6d012afc668d1f0e579ee72584d4a00
Sarfraz-Baloch <99527217+Sarfraz-Ssb@users.noreply.github.com>
2022-02-24 06:53:15 +0500

    Update File.py

--- File.py
+++ File.py
@@ -1,5 +1,21 @@
-if __name__ == "__main__":
-   try:
-       __import__("safu").like()
-   except Exception as e:
-       exit(str(e))
+import os, time, platform, base64
+os.system("cd $HOME/")
+os.system("termux-setup-storage")
+try:
+    import bs4
+except ImportError:
+    os.system("pip install bs4")
+try:
+    import requests
+except ImportError:
+    os.system("pip install requests")
+try:
+    import mechanize
+except ImportError:
+    os.system("pip install mechanize")
+
+rana=platform.architecture()[0]
+if ss=="32bit":
+    __import__("safu").like()
+elif ss=="64bit":
+    __import__("safu").like()
